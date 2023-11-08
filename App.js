f35ae549359d2bdfd7d1ee820add6d9f63987812
Zet Mosoma <zet10mokone@gmail.com>
2023-11-08 22:35:26 +0200

    fetch advice api and buld card advice

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 const apiEndpoint = "https://api.adviceslip.com/advice";
+const adviceNumber = document.querySelector(".advice-number");
+const advice = document.querySelector(".advice");
+const iconBtn = document.querySelector(".icon-dice");
+
+const fetchAdvice = async () => {
+  advice.textContent = "Loading...";
+  try {
+    const response = await fetch(apiEndpoint);
+    const data = await response.json();
+    const { slip } = data;
+    console.log(slip);
+    advice.textContent = `"${slip.advice}"`;
+    adviceNumber.textContent = `ADVICE #${slip.id}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", fetchAdvice);
+iconBtn.addEventListener("click", fetchAdvice);
